e5df700f1b3b29280a8a799faaf3f83344a0b15a
Jai Ram Rideout <jai.rideout@gmail.com>
2014-12-10 16:54:45 -0700

    INIT: add Greengenes 13_8 reference data files

---
+++ README.rst
@@ -0,0 +1,43 @@
+qiime-default-reference
+-----------------------
+
+|Build Status| |Coverage Status|
+
+qiime-default-reference, canonically pronounced *chime default reference*, is a
+Python package containing default reference data files for use with
+`QIIME <http://qiime.org/>`_. The current default reference data is compiled
+from the Greengenes 16S rRNA database version
+`13_8 <ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_8_otus.tar.gz>`_.
+
+Attribution
+^^^^^^^^^^^
+The reference data distributed in this Python package were copied from the
+`Greengenes 16S rRNA database <http://greengenes.secondgenome.com/>`_:
+
+**An improved Greengenes taxonomy with explicit ranks for ecological and
+evolutionary analyses of bacteria and archaea.**
+McDonald D, Price MN, Goodrich J, Nawrocki EP, DeSantis TZ, Probst A,
+Andersen GL, Knight R, Hugenholtz P.
+ISME J. 2012 Mar;6(3):610-8. doi: 10.1038/ismej.2011.139.
+
+The Greengenes reference data is licensed under a
+`Creative Commons Attribution-ShareAlike 3.0 Unported License <http://creativecommons.org/licenses/by-sa/3.0/deed.en_US>`_.
+
+The default template alignment column mask (i.e., "Lane mask") is derived from:
+
+Lane,D.J. (1991) **16S/23S rRNA sequencing.** In Stackebrandt,E. and
+Goodfellow,M. (eds), Nucleic Acid Techniques in Bacterial Systematics.
+John Wiley and Sons, New York, pp. 115-175.
+
+Lane mask was originally available in `ARB <http://www.ncbi.nlm.nih.gov/pubmed/14985472>`_.
+
+Lane mask taken from `here <http://greengenes.lbl.gov/Download/Sequence_Data/lanemask_in_1s_and_0s>`_.
+
+Getting Help
+^^^^^^^^^^^^
+Please post your questions about this repository/package on the `QIIME Forum <http://forum.qiime.org>`_.
+
+.. |Build Status| image:: https://travis-ci.org/biocore/qiime-default-reference.svg?branch=master
+   :target: https://travis-ci.org/biocore/qiime-default-reference
+.. |Coverage Status| image:: https://coveralls.io/repos/biocore/qiime-default-reference/badge.png
+   :target: https://coveralls.io/r/biocore/qiime-default-reference
---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+# Copied and modified from
+# https://github.com/biocore/scikit-bio/blob/master/setup.py
+# See licenses/scikit-bio.txt for more details.
+
+import sys
+import gzip
+import os.path
+from setuptools import find_packages, setup
+
+# if egg_info is passed as an argument do not preprocess (e.g., decompress) any
+# data
+preprocess_data = 'egg_info' not in sys.argv
+
+__version__ = "0.1.0-dev"
+
+classes = """
+    Development Status :: 3 - Alpha
+    Topic :: Scientific/Engineering :: Bio-Informatics
+    Programming Language :: Python
+    Programming Language :: Python :: 2
+    Programming Language :: Python :: 2.7
+    Operating System :: Unix
+    Operating System :: POSIX
+    Operating System :: MacOS :: MacOS X
+"""
+classifiers = [s.strip() for s in classes.split('\n') if s]
+
+description = 'Default reference data files for use with QIIME.'
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+def gunzip(fp):
+    out_fp = os.path.splitext(fp)[0]
+    sys.stdout.write("    Decompressing %s to %s..." % (fp, out_fp))
+    with gzip.open(fp, 'rb') as gzipped_fh:
+        with open(out_fp, 'wb') as out_fh:
+            for line in gzipped_fh:
+                out_fh.write(line)
+    sys.stdout.write("done\n")
+
+
+gg_module_prefix = 'qiime_default_reference.gg_13_8_otus'
+gg_path_prefix = gg_module_prefix.replace('.', os.path.sep)
+
+if preprocess_data:
+    sys.stdout.write("Decompressing reference data files:\n")
+    gunzip(os.path.join(gg_path_prefix, 'rep_set', '97_otus.fasta.gz'))
+    gunzip(os.path.join(gg_path_prefix, 'taxonomy', '97_otu_taxonomy.txt.gz'))
+    gunzip(os.path.join(gg_path_prefix, 'rep_set_aligned', '85_otus.fasta.gz'))
+
+setup(
+    name='qiime-default-reference',
+    version=__version__,
+    license='CC BY-SA 3.0',
+    description=description,
+    long_description=long_description,
+    author="QIIME development team",
+    author_email="qiime.help@gmail.com",
+    maintainer="QIIME development team",
+    maintainer_email="qiime.help@gmail.com",
+    url='http://www.qiime.org',
+    test_suite='nose.collector',
+    packages=find_packages(),
+    extras_require={'test': ['nose']},
+    classifiers=classifiers,
+    package_data={
+        gg_module_prefix : ['rep_set/*.fasta', 'taxonomy/*.txt',
+                            'rep_set_aligned/*.fasta']
+    }
+)
